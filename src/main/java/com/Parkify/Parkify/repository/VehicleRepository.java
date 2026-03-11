acf76df3b4689f332380449e0263460392ac1c32
IT24101654 <yasith.hasarinda2003@gmail.com>
2026-03-11 22:31:02 +0530

    Update src/main/java/com/Parkify/Parkify/repository/VehicleRepository.java

--- src/main/java/com/Parkify/Parkify/repository/VehicleRepository.java
+++ src/main/java/com/Parkify/Parkify/repository/VehicleRepository.java
@@ -1,4 +1,13 @@
 package com.Parkify.Parkify.repository;
 
-public interface VehicleRepository {
+import com.Parkify.Parkify.model.Vehicle;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
+    List<Vehicle> findByOwnerId(Long userId);
+    Optional<Vehicle> findByVehicleNumber(String vehicleNumber);
 }
