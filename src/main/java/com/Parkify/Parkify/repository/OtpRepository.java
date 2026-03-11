217f9342710aff3b2a8bd62f242d49bc3a481555
IT24101654 <yasith.hasarinda2003@gmail.com>
2026-03-11 22:31:02 +0530

    Update src/main/java/com/Parkify/Parkify/repository/OtpRepository.java

--- src/main/java/com/Parkify/Parkify/repository/OtpRepository.java
+++ src/main/java/com/Parkify/Parkify/repository/OtpRepository.java
@@ -1,4 +1,10 @@
 package com.Parkify.Parkify.repository;
 
-public class OtpRepository {
+import com.Parkify.Parkify.model.Otp;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface OtpRepository extends JpaRepository<Otp, Long> {
+    Optional<Otp> findByEmail(String email);
+    void deleteByEmail(String email);
 }
