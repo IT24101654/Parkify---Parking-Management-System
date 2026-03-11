ad70d1ac294fcddafb215b9dacf83d8cac23bad0
IT24101654 <yasith.hasarinda2003@gmail.com>
2026-03-11 22:31:01 +0530

    Update src/main/java/com/Parkify/Parkify/dto/LoginRequest.java

--- src/main/java/com/Parkify/Parkify/dto/LoginRequest.java
+++ src/main/java/com/Parkify/Parkify/dto/LoginRequest.java
@@ -1,4 +1,13 @@
 package com.Parkify.Parkify.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginRequest {
+    private String email;
+    private String password;
 }
