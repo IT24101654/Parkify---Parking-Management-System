7f459ffb4d90ee3a22526dfecf8307753ebc6476
IT24101654 <yasith.hasarinda2003@gmail.com>
2026-03-11 22:31:02 +0530

    Update src/main/java/com/Parkify/Parkify/service/UserServiceExtra.java

--- src/main/java/com/Parkify/Parkify/service/UserServiceExtra.java
+++ src/main/java/com/Parkify/Parkify/service/UserServiceExtra.java
@@ -1,4 +1,13 @@
 package com.Parkify.Parkify.service;
 
-public class UserServiceExtra {
+import com.Parkify.Parkify.model.ActivityLog;
+import com.Parkify.Parkify.model.FavoriteLocation;
+
+import java.util.List;
+
+public interface UserServiceExtra {
+    void logActivity(Long userId, String action);
+    List<ActivityLog> getUserLogs(Long userId);
+    FavoriteLocation addFavorite(Long userId, Long slotId);
+    List<FavoriteLocation> getFavorites(Long userId);
 }
