e240dcb936655808711bab0a3bdbc54efe026eef
IT24101654 <yasith.hasarinda2003@gmail.com>
2026-03-11 22:31:01 +0530

    Update src/main/java/com/Parkify/Parkify/model/Vehicle.java

--- src/main/java/com/Parkify/Parkify/model/Vehicle.java
+++ src/main/java/com/Parkify/Parkify/model/Vehicle.java
@@ -1,4 +1,34 @@
 package com.Parkify.Parkify.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "vehicles")
 public class Vehicle {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String vehicleNumber;
+
+    private String brand;
+    private String model;
+    private String type;
+    private String fuelType;
+
+    private String vehicleImage;
+    private String revenueLicenseImage;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    @JsonIgnore
+    private User owner;
 }
