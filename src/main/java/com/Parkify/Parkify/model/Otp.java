052462410dc131b97bcc15bfabc835e979e236c5
IT24101654 <yasith.hasarinda2003@gmail.com>
2026-03-11 22:31:01 +0530

    Update src/main/java/com/Parkify/Parkify/model/Otp.java

--- src/main/java/com/Parkify/Parkify/model/Otp.java
+++ src/main/java/com/Parkify/Parkify/model/Otp.java
@@ -1,4 +1,26 @@
 package com.Parkify.Parkify.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Otp {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+    private String otp;
+    private LocalDateTime expiryTime;
 }
