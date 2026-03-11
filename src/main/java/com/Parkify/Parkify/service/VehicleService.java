83635d20d220bb1b46f44d4ae1738a77a74116df
IT24101654 <yasith.hasarinda2003@gmail.com>
2026-03-11 22:31:03 +0530

    Update src/main/java/com/Parkify/Parkify/service/VehicleService.java

--- src/main/java/com/Parkify/Parkify/service/VehicleService.java
+++ src/main/java/com/Parkify/Parkify/service/VehicleService.java
@@ -1,4 +1,12 @@
 package com.Parkify.Parkify.service;
 
+import com.Parkify.Parkify.model.Vehicle;
+import java.util.List;
+
 public interface VehicleService {
+    Vehicle addVehicle(Long userId, Vehicle vehicle, String vImage, String lImage);
+    List<Vehicle> getVehiclesByUserId(Long userId);
+    Vehicle updateVehicle(Long vehicleId, Vehicle vehicleDetails);
+    void deleteVehicle(Long vehicleId);
+    Vehicle getVehicleById(Long vehicleId);
 }
