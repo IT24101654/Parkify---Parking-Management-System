4eae7e10d62a07142c6bd0bf86be3a852b030dda
IT24101654 <yasith.hasarinda2003@gmail.com>
2026-03-11 22:31:02 +0530

    Update src/main/java/com/Parkify/Parkify/service/JwtService.java

--- src/main/java/com/Parkify/Parkify/service/JwtService.java
+++ src/main/java/com/Parkify/Parkify/service/JwtService.java
@@ -1,4 +1,44 @@
 package com.Parkify.Parkify.service;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+
+@Service
 public class JwtService {
+
+    private final String SECRET = "mysecretkey_parkify_secure_system_2026_backend_dev";
+    private final Key SIGNING_KEY = Keys.hmacShaKeyFor(SECRET.getBytes());
+
+    public String generateToken(String email, String role) {
+        return Jwts.builder()
+                .setSubject(email)
+                .claim("role", role) // Token එක ඇතුළට Role එක දානවා
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
+                .signWith(SIGNING_KEY, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String extractEmail(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(SIGNING_KEY)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public String extractRole(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(SIGNING_KEY)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .get("role", String.class);
+    }
 }
