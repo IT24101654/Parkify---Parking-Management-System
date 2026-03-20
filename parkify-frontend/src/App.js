a0b977e6e9260bdbdb7d67a133cf71a1128f9fe4
IT24101654 <yasith.hasarinda2003@gmail.com>
2026-03-21 00:52:35 +0530

    Update parkify-frontend/src/App.js

--- parkify-frontend/src/App.js
+++ parkify-frontend/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css'; // App.css link කිරීම
+import './App.css';
 
 import LandingPage from './Pages/LandingPage';
 import Login from './Pages/Login';
-import Dashboard from './Pages/Dashboard';
+import Dashboard from './Pages/Super-Admin/Dashboard';
 import Register from "./Pages/Register";
+import PODashboard from './Pages/Parking-Owner/PODashboard'; // මේක හරියටම path එක බලන්න
 
 function App() {
     return (
@@ -14,7 +15,10 @@ function App() {
                 <Route path="/" element={<LandingPage />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
+                
+                {/* Role based Dashboards */}
                 <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/po-dashboard" element={<PODashboard />} />
             </Routes>
         </Router>
     );
