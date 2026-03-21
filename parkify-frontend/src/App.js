d70687cc590c010a43ce83476c78e71f895834e8
IT24101654 <yasith.hasarinda2003@gmail.com>
2026-03-21 22:12:03 +0530

    remove some unnecessary  things

--- parkify-frontend/src/App.js
+++ parkify-frontend/src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 
 import LandingPage from './Pages/LandingPage';
 import Login from './Pages/Login';
-import Dashboard from './Pages/Super-Admin/Dashboard';
 import Register from "./Pages/Register";
+
+import Dashboard from './Pages/Super-Admin/Dashboard';
+
 import PODashboard from './Pages/Parking-Owner/PODashboard';
+
 import Drdashboard from './Pages/Driver/Drdashboard';
-import ProtectedRoute from './Components/ProtectedRoute';
 
 function App() {
     return (
@@ -18,22 +20,13 @@ function App() {
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
                 
-                {/* Role based Dashboards */}
-                <Route path="/dashboard" element={
-                    <ProtectedRoute allowedRoles={['SUPER_ADMIN']}>
-                        <Dashboard />
-                    </ProtectedRoute>
-                } />
-                <Route path="/po-dashboard" element={
-                    <ProtectedRoute allowedRoles={['PARKING_OWNER']}>
-                        <PODashboard />
-                    </ProtectedRoute>
-                } />
-                <Route path="/dr-dashboard" element={
-                    <ProtectedRoute allowedRoles={['DRIVER']}>
-                        <Drdashboard />
-                    </ProtectedRoute>
-                } />
+              
+                <Route path="/admin-dashboard" element={<Dashboard />} />
+                
+                <Route path="/po-dashboard" element={<PODashboard />} />
+                
+                <Route path="/driver-dashboard" element={<Drdashboard />} />
+
             </Routes>
         </Router>
     );
